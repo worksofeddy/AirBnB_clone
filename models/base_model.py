147a399a0fb8bba97e0498e1f4a5a038dad0ee12
annmulwa <annmulwa2000@gmail.com>
2024-02-06 04:23:44 -0800

    Airbnb Clone

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,23 @@ class BaseModel:
     '''
     represents the base class
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         Initializes the base model class
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         '''
